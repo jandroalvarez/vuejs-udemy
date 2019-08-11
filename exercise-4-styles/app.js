4290ee816078e25aeebea829ca422b925f694092
jandroalvarez <alexalvarez87@gmail.com>
2019-08-11 18:18:23 +0200

    First part implemented

--- exercise-4-styles/app.js
+++ exercise-4-styles/app.js
@@ -1,11 +1,19 @@
 new Vue({
   el: '#exercise',
   data: {
-
+    activeEffect:''
   },
   methods: {
     startEffect: function() {
-    
+      var vm = this;
+    setInterval(() => {
+      if(vm.activeEffect == '')
+        vm.activeEffect = 'shrink';
+      else if(vm.activeEffect == 'shrink')
+        vm.activeEffect = 'highlight';
+      else
+        vm.activeEffect = 'shrink';        
+    }, 1000);
     }
   }
 });
